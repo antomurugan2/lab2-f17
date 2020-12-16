ba278df2197acbd390a7cd43df89d29e46e38d7e
antomurugan2 <63279320+antomurugan2@users.noreply.github.com>
2020-12-16 11:33:52 -0800

    finished shm_open function

--- shm.c
+++ shm.c
@@ -32,10 +32,44 @@ int shm_open(int id, char **pointer) {
 
 //you write this
 
+  // Acquire lock on the shm table
+  acquire(&(shm_table.lock));
+  struct proc* currProc = myproc();
 
+  // Find the entry matching the id
+  for(int i = 0; i < 64; i++) {
+    //  If the entry is found then a process has already allocated a physical page 
+    // Increment the reference count of that page
+    if(shm_table.shm_pages[i].id == id) {
+      char* memory = shm_table.shm_pages[i].frame;
+      uint vir = PGROUNDUP(currProc->sz);
+      mappages(currProc->pgdir, (void*)vir, PGSIZE, V2P(memory), PTE_W|PTE_U);
+      shm_table.shm_pages[i].refcnt++;
+      *pointer = (char*) vir;
+      currProc->sz = PGROUNDUP(currProc->sz) + PGSIZE; 
 
+      // Release lock on the shm_table
+      release(&(shm_table.lock));
+      return 0;
+    }
+  }
+  
+  // No entry is found
+  shm_table.shm_pages[id-1].id = id;
+  shm_table.shm_pages[id-1].frame = kalloc(); 
+  memset(shm_table.shm_pages[id-1].frame, 0, PGSIZE);
 
-return 0; //added to remove compiler warning -- you should decide what to return
+  
+  char* memory = shm_table.shm_pages[id-1].frame;
+  uint vir = PGROUNDUP(currProc->sz);
+  mappages(currProc->pgdir, (void*)vir, PGSIZE, V2P(memory), PTE_W|PTE_U);
+  shm_table.shm_pages[id-1].refcnt = 1;
+  *pointer = (char*) vir;
+  currProc->sz = PGROUNDUP(currProc->sz) + PGSIZE; 
+  
+  // Release lock on the shm_table
+  release(&(shm_table.lock));
+  return 0; //added to remove compiler warning -- you should decide what to return
 }
 
 
